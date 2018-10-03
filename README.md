48fc7fc1af8180500c99e0c9965b31099a07157c
Ogi <ognjen.bekic@gmail.com>
2018-10-03 21:11:52 +0200

    Example project to demonstrate digi.me SDK usage This project utilises the digi.me sdk to demonstrate consent access using digi.me

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # digime-js-sdk-example
+
+---
+## Preparation
+In order to run this example app, you'll need to perform some preparation steps:
+
+#### Obtain the following from digi.me:
+* Contract ID
+* Application ID
+* Private key (PKCS1 PEM)
+* Depending on which consent mode you run, you may need a valid Digi.me client application on your phone/desktop, with a library created on it ([See below for more info](#setting-up-the-digi.me-library))
+
+#### On the machine that will run the example:
+* Node version 8.6 or greater, with NPM
+* git (optional)
+
+## Installation
+1. [Download](https://github.com/digime/digime-js-sdk-example/archive/sdk.zip) and extract, or clone the `sdk` branch on this repo with the following command:
+
+    `git clone --single-branch -b sdk git@github.com:digime/digime-js-sdk-example.git`
+
+2. In your terminal, navigate to the directory where you cloned/extracted this example (If you see this README in it, you're in the correct place!)
+
+3. Run `npm install` in your terminal
+
+4. Open the `index.js` file in your favourite code editor, and replace or adjust environment variable with values you received from digi.me (described in code comments as well).
+
+5. Run `npm start` in your terminal
+
+    This should print out something along these lines:
+
+    ```Example app now running on:
+    - http://localhost:8081
+    - http://192.168.0.10:8081 (probably)
+    ```
+
+6. You should now be able to access the example app 🎉:
+    - First URL should be able to be opened on the same machine you started the server (So you can be sure it's running)
+    - The second URL is our best guess as to what your local network IP is. You should be able to open the example app from your phone with it, assuming that both machines are on the same network.
+
+## Additional information
+
+### Two user consent flows
+
+There are two ways for users to give consent to your digi.me library:
+A. **Using the official digi.me application (recommended)** - You will need to have the digi.me client installed to run this flow. Install the digi.me client from [here](https://digi.me/get-started) and create a library with some data that we can share. If you would like a pre-populated library, you can connect to one of our existing libraries, please contact digi.me for details.
+B. **Guest consent (demo)** - We can offer a guest consent flow which asks for the user data from the browser. As part of this flow, the users will need to go through the adding of services to provide the data to be shared to your application. When the user data is ready, the callback URL will be called and you can then request user data using our SDK. This flow is currently in demo mode with improvements to come in the coming months.
+
+### Going through the example - A. Native Application Flow
+
+You are able to switch between the different user flows by either calling `getAppURL` (native application flow) or `getWebURL` (guest consent flow) from the SDK.
+1. **Navigate to the example app** - Once you open up the second URL on your device, you should see a screen that has a button that says `Launch Native App Consent Request`
+
+2. **Provide consent** - If you have the digi.me client installed, you should be able to complete the consent access flow. Click on the `Launch Consent Request` button in the starting page. It should bring up the digi.me client and prompt you for consent. Once the user gives consent, the user should be taken to a new page on the browser which displays `Thank you for your data!`
+
+3. **See data shared** - In the last step of the example application, you'll be taken to an end page where we will list all the data that we've received from the user. You should see the number of items that have been shared, all the objects listed per file, and a description of what kind of data has been listed under each file.
+
+### Going through the example - B. Guest Consent Flow
+
+You are able to switch between the different user flows by either calling `getAppURL` (mobile flow) or `getWebURL` (guest consent flow) from the SDK. By default, this example uses the guest consent flow.
+1. **Navigate to the example app** - Once you open up the second URL on your device, you should see a screen that has a button that says `Launch Web Consent Request`
+
+2. **Provide consent** - Click on the `Launch Web Consent Request` button in the starting page. It should bring up digi.me consent page in another browser tab. From this flow you should see the contract details. The user will need to onboard different services to provide the data that can be passed back to the example application. Once the services are onboarded, the user will be asked to consent to sharing their data. Upon given the consent, the user should be taken to a new page on the browser which displays `Thank you for your data!`
+
+3. **See data shared** - In the last step of the example application, we will list all the data that we've received from the user. You should see the number of items that have been shared, all the objects listed per file, and a description of what kind of data has been listed under each file.
---
+++ index.js
@@ -0,0 +1,145 @@
+const express = require("express");
+const url = require("url");
+const trimEnd = require("lodash.trimend");
+
+const getBasePath = (req) => process.env.DIGIME_SDK_CALLBACK_URL_BASE || url.format({
+    protocol: req.protocol,
+    host: req.headers.host,
+    pathname: trimEnd(req.originalUrl, "/"),
+});
+
+// Some setup for the Express server
+const app = express();
+const port = 8081;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+// If you have no need to modify init options, you should just import the necessary funcitons directly like so:
+// const { establishSession, getAppURL, getDataForSession } = require("digime-js-sdk");
+
+// In this case, we're creating a new instance of SDK here because we want to specifiy different initialization options.
+const { createSDK } = require("digime-js-sdk");
+const { establishSession, getWebURL, getDataForSession, getAppURL } = createSDK({ host: process.env.DIGIME_SDK_HOST });
+
+// Options that we will pass to the Digi.me SDK
+const APP = {
+
+    // Replace this value with the Application ID that was provided to you by Digi.me
+    // We store our value for app_id in environment variable called DIGIME_SDK_APP_ID
+    // If you store it to same variable this does not need to be changed
+    appId: process.env.DIGIME_SDK_APP_ID,
+
+    // Replace this value with the Contract ID that was provided to you by Digi.me
+    // We store our value for contract_id in environment variable called DIGIME_SDK_CONTRACT_ID
+    // If you store it to same variable this does not need to be changed
+    contractId: process.env.DIGIME_SDK_CONTRACT_ID,
+
+    // We store our private key in in environment variable called DIGIME_SDK_PRIVATE_KEY
+    // If you store entire key into same variable this does not need to be changed
+    key: process.env.DIGIME_SDK_PRIVATE_KEY,
+};
+
+// In this route, we are presenting the user with an action that will take them to digi.me
+app.get('/', (req, res) => {
+
+    // First thing to do is to establish a session by using our Application ID and Contract ID
+    establishSession(APP.appId, APP.contractId).then((session) => {
+
+        /*
+         * Once the session is established we have two options here:
+         * we can pass the Application ID, session and a callback URL
+         * to the "getAppUrl" to get a link to which you will need to direct the user to.
+         * or we pass the session and a callback URL
+         * to the "getWebURL" to get a web link to which you will need to direct the user to.
+         *
+         * With regards to the callback URL:
+         *
+         * The route for it is created later on in this file.
+         * You should probably lock this down in your own production code,
+         * but for demonstation purposes we're just blindly using the hostname from the request,
+         * which you probably shouldn't do.
+         *
+         * We're also passing in the session key, so that we know what session to retrieve the data from
+         * when the user returns from Digi.me. You can make this URL contain any additional info you want,
+         * for example, the users ID in your system, or something like that.
+         *
+         * When the Digi.me flow is complete, the user will be directed to this URL, with one of the following
+         * added to the query string:
+         *
+         * - result=DATA_READY (User has approved our request, and data is available to be retrieved)
+         * - result=CANCELLED (User has denied our request)
+         *
+         */
+        const weburl = getWebURL(
+            session,
+            `${getBasePath(req)}/return?sessionId=${session.sessionKey}`
+        );
+
+        const appurl = getAppURL(
+            APP.appId,
+            session,
+            `${getBasePath(req)}/return?sessionId=${session.sessionKey}`
+        );
+
+        // Present the generated URL with a pretty template
+        res.render('pages/index', { weburl, appurl });
+    });
+});
+
+// Here we are creating the return path, that was mentioned earlier and was passed to the `getAppURL` function.
+app.get("/return", (req, res) => {
+
+    // This is the result of consent request that was sent to Digi.me
+    const result = req.query.result;
+
+    let consentdata = [];
+    let totalItems = 0;
+    // If we did not get the response that the consent was APPROVED, there's not much we can do,
+    // so we're just gonna stop and show an sad error page. :(
+    if (result !== "DATA_READY") {
+        res.render('pages/error');
+        return;
+    }
+
+    // If we get do get the information that the consent request was APPROVED,
+    // the data is ready to be retrieved and consumed!
+
+    // Here, we're using `getDataForSession` to retrieve the data from Digi.me API,
+    // by using the Session ID and the Application ID.
+    //
+    // Additionally, we're passing in the function that will receive data from the retrieved files.
+    // This function serves as a callback function that will be called for each file, with the decrypted data,
+    // after it was retrieved and decrypted with your key.
+    const data = getDataForSession(
+        req.query.sessionId, // Our Session ID that we retrieved from the URL
+        APP.key, // The private key we setup above
+        ({fileData, fileName, fileDescriptor}) => {
+            // This is where you deal with any data you receive from Digi.me,
+            // in this case, we're just printing it out to the console.
+            // You probably have a better idea on what to do with it! :)
+            totalItems += fileData.length;
+            consentdata.push({
+                fileName,
+                fileDescriptor,
+                fileData
+            });
+        },
+        ({fileName, error}) => console.log(`Error retrieving file ${fileName}: ${error.toString()}`),
+    );
+
+    // `getDataForSession` returns a promise that will resolve once every file was processed.
+    // Again, we're just logging to console here as an example
+    data.then(() => {
+        // And we're just presenting a nice page here, thanking the user for their data!
+        res.render('pages/return', { consentdata, totalItems });
+    });
+});
+
+app.listen(port, () => {
+    console.log([
+        "Example app now running on:",
+        `- http://localhost:${port}`,
+        `- http://${require("ip").address()}:${port} (probably)`,
+    ].join("\n"));
+});
